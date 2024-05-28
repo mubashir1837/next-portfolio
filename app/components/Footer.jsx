5ee5b0e640b612019eb1ea8d3bf2e3d78875a637
My Name <someone@email.com>
2024-05-28 16:35:19 +0500

    portfolio

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaMapMarkerAlt, FaEnvelope, FaLinkedin, FaGithub, FaInstagram, FaFacebook, FaTwitter } from 'react-icons/fa';
+import { FaMapMarkerAlt, FaEnvelope, FaLinkedin, FaGithub, FaInstagram, FaFacebook, FaTwitter, FaWhatsapp } from 'react-icons/fa';
 import Image from 'next/image';
 
 const Footer = () => {
@@ -7,22 +7,22 @@ const Footer = () => {
     <section className="min-h-screen flex flex-col items-center justify-center px-6 py-24">
       <div className="flex flex-col items-center lg:flex-row lg:justify-between lg:items-center w-full max-w-7xl mx-auto">
         <div className="text-center lg:text-left lg:max-w-2xl">
-          <h1 className="text-4xl sm:text-5xl lg:text-6xl font-extrabold mb-6 text-transparent bg-clip-text bg-gradient-to-r from-teal-200 to-teal-100">
+          <h3 className="text-4xl sm:text-5xl lg:text-6xl font-extrabold mb-6 text-transparent bg-clip-text bg-gradient-to-r from-teal-200 to-teal-100">
             Get in Touch!
-          </h1>
-          <p className="text-lg sm:text-xl lg:text-lg text-white text-center lg:text-left mb-6 lg:max-w-lg">
+          </h3>
+          <p className="text-lg sm:text-xl lg:text-lg text-white text-center lg:text-left mb-6 lg:max-w-6xl">
             I am currently exploring exciting new opportunities and eagerly anticipate receiving thoughtful messages in my inbox. Whether you have inquiries, potential job opportunities, or simply wish to connect, rest assured, I will respond promptly and warmly!
           </p>
-          <p className="text-md sm:text-lg lg:text-md text-white text-center lg:text-left mb-12 lg:max-w-lg">
+          <p className="text-md sm:text-lg lg:text-md text-white text-center lg:text-left mb-12 lg:max-w-5xl">
             An enthusiastic undergraduate student, passionate about Bioinformatics, Biopython, Python, JavaScript, HTML, and CSS.
           </p>
           <div className="flex items-center justify-center lg:justify-start mb-8">
             <FaMapMarkerAlt className="text-white text-2xl mr-2" />
             <span className="text-white text-lg">Islamabad, Pakistan</span>
           </div>
-          <div className="flex space-x-4 justify-center lg:justify-start">
+          <div className="flex space-x-4 justify-center lg:justify-start size-max-0px">
             <a href="mailto:your-email@example.com" className="text-white text-3xl">
-              <FaEnvelope />
+              <FaWhatsapp />
             </a>
             <a href="https://www.linkedin.com/in/your-linkedin-profile" className="text-white text-3xl">
               <FaLinkedin />
@@ -42,7 +42,7 @@ const Footer = () => {
           </div>
         </div>
         <div className="mt-12 lg:mt-0">
-          <img src="/images/pro.jpg" alt="GitHub Profile Photo" className="w-40 h-40 rounded-full mx-auto lg:mx-0" />
+          <img src="/images/pro.jpg" alt="GitHub Profile Photo" className="w-200 h-auto rounded-full mr-6 lg:mx-0" />
         </div>
       </div>
     </section>
