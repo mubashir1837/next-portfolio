d07e7fcbbfbf6f3a6bf7443a571e1dd95b31be27
My Name <someone@email.com>
2024-05-28 12:12:04 +0500

    portfolio

--- app/components/Skills.jsx
+++ app/components/Skills.jsx
@@ -10,7 +10,7 @@ const Skills = () => {
         description="A list of my skills and technologies I'm proficient in."
         keywords="skills, technologies, proficiency Planner, Ahrefs, SEMrush,"
       />
-      <section className="min-h-screen px-4 py-12 ">
+      <section className="min-h-screen px-4 py-24 bg-gradient-to-r from-[#010524] to-[#040c5c]">
         <h1 className="text-4xl sm:text-5xl lg:text-6xl font-bold mb-6 text-transparent bg-clip-text text-center bg-gradient-to-r from-[#19e345] to-[#fff]">
           Skills
         </h1>
@@ -19,44 +19,44 @@ const Skills = () => {
             <div className="flex flex-wrap -mx-4">
               <div className="w-full lg:w-1/12"></div>
               <div className="w-full lg:w-5/12 px-4">
-                <p className="text-lg">
-                  I <span className="text-green-500">design and develop services</span> for customers of all sizes, specializing in creating stylish, modern websites, web services, and online stores. I created many projects related to Bioinformatics <span className="text-green-500">by using deep learning and machine learning.</span> Here are skills I&apos;ve acquired too.
+                <p className="text-lg text-white">
+                  I design and develop services for customers of all sizes, specializing in creating stylish, modern websites, web services, and online stores. I created many projects related to Bioinformatics by using deep learning and machine learning. Here are skills I&apos;ve acquired too.
                 </p>
               </div>
               <div className="w-full lg:w-5/12 px-4 mt-8 lg:mt-0">
                 <div className="mb-4">
                   <div className="flex justify-between items-center">
-                    <p className="text-lg">Python</p>
-                    <span className="text-lg">95%</span>
+                    <p className="text-lg text-white">Python</p>
+                    <span className="text-lg text-white">95%</span>
                   </div>
-                  <div className="w-full bg-gray-700 rounded-full h-2.5">
-                    <div className="bg-green-500 h-2.5 rounded-full" style={{ width: '95%' }}></div>
+                  <div className="w-full bg-white rounded-full h-2.5">
+                    <div className="bg-[#28b6bb] h-2.5 rounded-full" style={{ width: '95%' }}></div>
                   </div>
                 </div>
                 <div className="mb-4">
                   <div className="flex justify-between items-center">
-                    <p className="text-lg">JavaScript</p>
-                    <span className="text-lg">75%</span>
+                    <p className="text-lg text-white">JavaScript</p>
+                    <span className="text-lg text-white">75%</span>
                   </div>
-                  <div className="w-full bg-gray-700 rounded-full h-2.5">
+                  <div className="w-full bg-white rounded-full h-2.5">
                     <div className="bg-green-500 h-2.5 rounded-full" style={{ width: '75%' }}></div>
                   </div>
                 </div>
                 <div className="mb-4">
                   <div className="flex justify-between items-center">
-                    <p className="text-lg">HTML</p>
-                    <span className="text-lg">89%</span>
+                    <p className="text-lg text-white">HTML</p>
+                    <span className="text-lg text-white">89%</span>
                   </div>
-                  <div className="w-full bg-gray-700 rounded-full h-2.5">
+                  <div className="w-full bg-white rounded-full h-2.5">
                     <div className="bg-green-500 h-2.5 rounded-full" style={{ width: '89%' }}></div>
                   </div>
                 </div>
                 <div className="mb-4">
                   <div className="flex justify-between items-center">
-                    <p className="text-lg">CSS</p>
-                    <span className="text-lg">89%</span>
+                    <p className="text-lg text-white">CSS</p>
+                    <span className="text-lg text-white">89%</span>
                   </div>
-                  <div className="w-full bg-gray-700 rounded-full h-2.5">
+                  <div className="w-full bg-white rounded-full h-2.5">
                     <div className="bg-green-500 h-2.5 rounded-full" style={{ width: '89%' }}></div>
                   </div>
                 </div>
@@ -67,39 +67,39 @@ const Skills = () => {
         <div className="py-12">
           <div className="container mx-auto px-4">
             <div className="flex flex-wrap -mx-4 justify-center">
-              <div className="w-full md:w-1/2 lg:w-1/4 px-4 mb-8">
+              <div className="w-full sm:w-1/2 lg:w-1/4 px-4 mb-8">
                 <div className="text-center">
                   <span className="block mb-2">
                     <i className="fa fa-hand-peace-o text-green-500"></i>
                   </span>
-                  <h5 className="text-4xl">3</h5>
+                  <h5 className="text-4xl text-white">3</h5>
                   <p className="text-gray-400">Total Experience</p>
                 </div>
               </div>
-              <div className="w-full md:w-1/2 lg:w-1/4 px-4 mb-8">
+              <div className="w-full sm:w-1/2 lg:w-1/4 px-4 mb-8">
                 <div className="text-center">
                   <span className="block mb-2">
                     <i className="fa fa-smile-o text-green-500"></i>
                   </span>
-                  <h5 className="text-4xl">300</h5>
+                  <h5 className="text-4xl text-white">300</h5>
                   <p className="text-gray-400">Happy Clients</p>
                 </div>
               </div>
-              <div className="w-full md:w-1/2 lg:w-1/4 px-4 mb-8">
+              <div className="w-full sm:w-1/2 lg:w-1/4 px-4 mb-8">
                 <div className="text-center">
                   <span className="block mb-2">
                     <i className="fa fa-thumbs-up text-green-500"></i>
                   </span>
-                  <h5 className="text-4xl">100</h5>
+                  <h5 className="text-4xl text-white">100</h5>
                   <p className="text-gray-400">Project Complete</p>
                 </div>
               </div>
-              <div className="w-full md:w-1/2 lg:w-1/4 px-4 mb-8">
+              <div className="w-full sm:w-1/2 lg:w-1/4 px-4 mb-8">
                 <div className="text-center">
                   <span className="block mb-2">
                     <i className="fa fa-trophy text-green-500"></i>
                   </span>
-                  <h5 className="text-4xl">50</h5>
+                  <h5 className="text-4xl text-white">50</h5>
                   <p className="text-gray-400">Award Win</p>
                 </div>
               </div>
