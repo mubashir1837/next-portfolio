6fd07a2f9929ae0515cce76697af7ffc33f9936d
Mubashir Ali <mubashirali1837@gmail.com>
2024-06-07 17:04:24 +0500

    portfolio

--- app/layout.js
+++ app/layout.js
@@ -25,6 +25,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+       <head>
+        <link rel="icon" href="./images/pro-.jpg" sizes="any" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
